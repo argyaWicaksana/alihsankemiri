e4b3c421ff416d511d763de7853bb14016081242
argyaWicaksana <argya7@gmail.com>
2022-02-19 12:39:27 +0700

    membuat navbar

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function noBorder() {
     document.getElementById('searchBox').style.border = 'none'
 }
+
+function toggle() {
+    let x = document.getElementById('toggle')
+    if (x.className =="on") { //tutup
+       x.classList.remove("on") 
+        document.getElementById('menuResp').style.display = ''
+        document.getElementById('menuResp').style.width = ''
+    } else { //buka
+        x.className+="on"
+        document.getElementById('menuResp').style.display = 'block'
+        document.getElementById('menuResp').style.width = '100%'
+    }
+    
+}
